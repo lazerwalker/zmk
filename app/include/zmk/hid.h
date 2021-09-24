5ab17fbf66261062329e62866468f7be2bffa846
dnaq <dnaq@users.noreply.github.com>
2021-09-24 17:53:17 +0200

    feat(stenography): Support the Plover HID Protocol
    This commit adds tentative support for the Plover HID Protocol, a
    simple HID-based protocol that works over both USB and BLE.
    The main value-add of this feature is that it allows ZMK to be used for
    interfacing with Plover from the Open Steno Project without Plover
    having to intercept all keyboard keypresses.
    
    Usually hobbyist steno machines use legacy serial port protocols for
    this, but since we can't use those over BLE we might as well develop a
    protocol that works both for USB and BLE and at the same time remove
    some of the pain points of those old protocols.

--- app/include/zmk/hid.h
+++ app/include/zmk/hid.h
@@ -101,6 +101,18 @@
 
 #define HID_USAGE16_SINGLE(a) HID_USAGE16((a & 0xFF), ((a >> 8) & 0xFF))
 
+#define ZMK_HID_KEYBOARD_NKRO_SIZE 6
+
+#define ZMK_HID_CONSUMER_NKRO_SIZE 6
+
+#define ZMK_HID_PLOVER_SIZE 8
+
+// As a workaround for limitations in how some operating systems expose hid
+// descriptors to user level code the Plover HID protocol hard codes a report
+// id of 0x50 so that the plover side can distinguish between Plover HID
+// reports and other reports from the device.
+#define PLOVER_HID_REPORT_ID 0x50
+
 static const uint8_t zmk_hid_report_desc[] = {
     HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP),
     HID_USAGE(HID_USAGE_GD_KEYBOARD),
@@ -251,8 +263,32 @@ static const uint8_t zmk_hid_report_desc[] = {
     HID_INPUT(ZMK_HID_MAIN_VAL_DATA | ZMK_HID_MAIN_VAL_VAR | ZMK_HID_MAIN_VAL_REL),
     HID_END_COLLECTION,
     HID_END_COLLECTION,
-    HID_END_COLLECTION,
 #endif // IS_ENABLED(CONFIG_ZMK_POINTING)
+
+#if IS_ENABLED(CONFIG_ZMK_PLOVER_HID)
+    /* USAGE (\xffPLV) */
+    HID_ITEM(HID_ITEM_TAG_USAGE_PAGE, HID_ITEM_TYPE_GLOBAL, 2), 0x50, 0xff,
+    HID_ITEM(HID_ITEM_TAG_USAGE, HID_ITEM_TYPE_LOCAL, 2), 0x56, 0x4c,
+    HID_COLLECTION(HID_COLLECTION_APPLICATION),
+    /* REPORT ID (80) */
+    HID_REPORT_ID(PLOVER_HID_REPORT_ID),
+    /* LOGICAL MINIMUM (0) */
+    HID_LOGICAL_MIN8(0x00),
+    /* LOGICAL MAXIMUM (1) */
+    HID_LOGICAL_MAX8(0x01),
+    /* REPORT SIZE (1) */
+    HID_REPORT_SIZE(0x01),
+    /* REPORT COUNT (64) */
+    HID_REPORT_COUNT(0x40),
+    /* USAGE PAGE (Ordinal) */
+    HID_USAGE_PAGE(0x0A),
+    /* USAGE MINIMUM (0) */
+    HID_USAGE_MIN8(0x00),
+    /* USAGE MAXIMUM (63) */
+    HID_USAGE_MAX8(63),
+    /* INPUT (Cnst,Var,Abs) */
+    HID_INPUT(0x02), HID_END_COLLECTION
+#endif /* IS_ENABLED(CONFIG_ZMK_PLOVER_HID) */
 };
 
 #if IS_ENABLED(CONFIG_ZMK_USB_BOOT)
@@ -345,6 +381,15 @@ struct zmk_hid_mouse_resolution_feature_report {
 
 #endif // IS_ENABLED(CONFIG_ZMK_POINTING)
 
+struct zmk_hid_plover_report_body {
+    uint8_t buttons[ZMK_HID_PLOVER_SIZE];
+} __packed;
+
+struct zmk_hid_plover_report {
+    uint8_t report_id;
+    struct zmk_hid_plover_report_body body;
+} __packed;
+
 zmk_mod_flags_t zmk_hid_get_explicit_mods(void);
 int zmk_hid_register_mod(zmk_mod_t modifier);
 int zmk_hid_unregister_mod(zmk_mod_t modifier);
@@ -394,3 +439,9 @@ zmk_hid_boot_report_t *zmk_hid_get_boot_report();
 #if IS_ENABLED(CONFIG_ZMK_POINTING)
 struct zmk_hid_mouse_report *zmk_hid_get_mouse_report();
 #endif // IS_ENABLED(CONFIG_ZMK_POINTING)
+
+int zmk_hid_plover_press(zmk_key_t key);
+int zmk_hid_plover_release(zmk_key_t key);
+void zmk_hid_plover_clear();
+
+struct zmk_hid_plover_report *zmk_hid_get_plover_report();
--- app/src/hid.c
+++ app/src/hid.c
@@ -33,6 +33,8 @@ static struct zmk_hid_mouse_report mouse_report = {
 
 #endif // IS_ENABLED(CONFIG_ZMK_POINTING)
 
+static struct zmk_hid_plover_report plover_report = {.report_id = PLOVER_HID_REPORT_ID, .body = {.buttons = {0}}};
+
 // Keep track of how often a modifier was pressed.
 // Only release the modifier if the count is 0.
 static int explicit_modifier_counts[8] = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -347,6 +349,8 @@ int zmk_hid_press(uint32_t usage) {
         return zmk_hid_keyboard_press(ZMK_HID_USAGE_ID(usage));
     case HID_USAGE_CONSUMER:
         return zmk_hid_consumer_press(ZMK_HID_USAGE_ID(usage));
+    case HID_USAGE_VENDOR_PLOVER & 0xFF:
+        return zmk_hid_plover_press(ZMK_HID_USAGE_ID(usage));
     }
     return -EINVAL;
 }
@@ -357,6 +361,8 @@ int zmk_hid_release(uint32_t usage) {
         return zmk_hid_keyboard_release(ZMK_HID_USAGE_ID(usage));
     case HID_USAGE_CONSUMER:
         return zmk_hid_consumer_release(ZMK_HID_USAGE_ID(usage));
+    case HID_USAGE_VENDOR_PLOVER & 0xFF:
+        return zmk_hid_plover_release(ZMK_HID_USAGE_ID(usage));
     }
     return -EINVAL;
 }
@@ -468,9 +474,23 @@ void zmk_hid_mouse_clear(void) {
 
 #endif // IS_ENABLED(CONFIG_ZMK_POINTING)
 
+int zmk_hid_plover_press(zmk_key_t code) {
+    plover_report.body.buttons[code / 8] |= (1 << (7 - (code % 8)));
+    return 0;
+};
+
+int zmk_hid_plover_release(zmk_key_t code) {
+    plover_report.body.buttons[code / 8] &= ~(1 << (7 - (code % 8)));
+    return 0;
+};
+
+void zmk_hid_plover_clear() { memset(&plover_report.body, 0, sizeof(plover_report.body)); }
+
 struct zmk_hid_keyboard_report *zmk_hid_get_keyboard_report(void) { return &keyboard_report; }
 
 struct zmk_hid_consumer_report *zmk_hid_get_consumer_report(void) { return &consumer_report; }
+
+struct zmk_hid_plover_report *zmk_hid_get_plover_report() { return &plover_report; }
 
 #if IS_ENABLED(CONFIG_ZMK_POINTING)
 
